a291d3f7a362e8af060c8faf6e5119a1876d24f7
Jon Lindell <jhlindell@yahoo.com>
2017-05-05 14:50:21 -0600

    got a lot working. first save

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-var foo = 1;
+$('#addButton').on('click', addRecipeCard);
+var $recipes = $('#recipes');
+var recipeCounter = 1;
+
+function addRecipeCard(event) {
+  if (recipeCounter < 6) {
+    var $clone = $("#cardClone").clone();
+    $clone.toggle();
+    $clone.removeAttr("id");
+    $clone.appendTo($recipes);
+    recipeCounter++;
+    if(recipeCounter === 6) {
+      $("#addButton").toggle();
+    }
+  }
+
+}
