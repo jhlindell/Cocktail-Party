8f51d8dece84159f5b25f897b6865b0bdec67595
Jon Lindell <jhlindell@yahoo.com>
2017-05-06 18:14:20 -0600

    update recipe mostly working.

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-$('#addButton').on('click', addRecipeCard);
-$('#addIngButton').on('click', addIngredient);
-var $recipes = $('#recipes');
-var $ingredients = $('#ingredients');
+$("#addRecipeButton").on("click", addRecipeCard);
+$("#addIngButton").on("click", addIngredient);
+$("#recipeBox").on("click", updateRecipe);
+var $recipes = $("#recipes");
+$recipes.on("click", recipeCardClick);
+var $ingredients = $("#ingredients");
+var $recipeBox = $(".recipeBox");
 var recipeCounter = 1;
 
 function addRecipeCard(event) {
@@ -12,7 +15,7 @@ function addRecipeCard(event) {
     $clone.appendTo($recipes);
     recipeCounter++;
     if(recipeCounter === 6) {
-      $("#addButton").toggle();
+      $("#addRecipeButton").toggle();
     }
   }
 }
@@ -22,4 +25,64 @@ function addIngredient(event) {
   $clone.toggle();
   $clone.removeAttr("id");
   $clone.appendTo($ingredients);
+}
+
+function recipeCardClick(event) {
+  if($(event.target).hasClass("deleteButton")){
+    removeCard(event);
+  }
+  if($(event.target).hasClass("updateRecipe")) {
+    selectRecipeCard(event);
+  }
+}
+
+function removeCard(event) {
+  let $target = $(event.target).parent().parent().parent().parent();
+  $target.remove();
+  recipeCounter--;
+}
+
+function selectRecipeCard(event){
+  let $target = $(event.target).parent().parent().parent();
+  $target.removeClass("panel-default");
+  $target.addClass("panel-primary");
+  $($recipeBox).toggle();
+  $(event.target).toggle();
+}
+
+function updateRecipe(event){
+  if($(event.target).hasClass("updateRecipeButton")){
+    let $selectedCard = $recipes.find(".panel-primary");
+    $selectedCard.find(".updateRecipe").toggle();
+    let recipe = buildRecipeObject();
+    populateCard(recipe);
+  }
+}
+
+function buildRecipeObject(){
+  let recipeObj = {};
+  recipeObj.name = $("#drinkName").val();
+  recipeObj.instructions = $("#instructions").val();
+  recipeObj.description = $("#description").val();
+  recipeObj.ingredients = [];
+  let $ingredients = $("#ingredients").children();
+  // $($ingredients).each(function(){
+  //   let ingredientObj = {};
+  //   ingredientObj.measure = $(this).find(".measure").val();
+  //   ingredientObj.ingredientName = $(this).find(".ingName").val();
+  //   ingredientObj.unit = "oz";
+  //   recipeObj.ingredients.push(ingredientObj);
+  // });
+  for(let i = 0; i < $ingredients.length; i++){
+    let ingredientObj = {};
+    ingredientObj.measure = $($ingredients[i]).find(".measure").val();
+    ingredientObj.ingredientName = $($ingredients[i]).find(".ingName").val();
+    ingredientObj.unit = "oz";
+    recipeObj.ingredients.push(ingredientObj);
+  }
+  return recipeObj;
+}
+
+function populateCard(recipe){
+  
 }
